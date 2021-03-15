939ddf3c7faeb957120fdb770a90a52bcfa09a0e
Michal <goldenofheart@gmail.com>
2021-03-16 00:28:40 +0100

    add about component

--- src/Main.js
+++ src/Main.js
@@ -2,35 +2,40 @@ import { Container } from 'react-grid-system'
 import Skills from './components/skills/index'
 import Projects from './components/projects/index'
 import Parallax from './components/parallax/index'
+import About from './components/about/index'
 import './App.css';
 import { useState, useEffect } from 'react';
 import ScrollTrigger from 'gsap/ScrollTrigger'
 import gsap from 'gsap'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+
 
 function Main() {
   const [skills, setSkills] = useState(false)
   const [projects, setProjects] = useState('')
   const [parallax, setParallax] = useState(false)
+  const [about, setAbout] = useState(false)
 
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
-
     gsap.to('section', {
       scrollTrigger: {
-        trigger: '.skills',
+        trigger: '.parallax',
         once: true,
-
+        start: 'top 20%',
+        onUpdate: () => {
+          setParallax(true)
+        }
+      }
+    })
+    gsap.to('section', {
+      scrollTrigger: {
+        trigger: '.about',
+        once: true,
         start: 'top 20%',
         markers: { startColor: "green", endColor: "yellow", fontSize: "12px" },
-        onEnter: () => {
-            setSkills('Frontend')
+        onUpdate: () => {
+          setAbout(true)
         }
       }
     })
@@ -39,23 +44,25 @@ function Main() {
         trigger: '.projects',
         once: true,
         start: 'top 40%',
-        markers: { startColor: "green", endColor: "yellow", fontSize: "12px" },
         onUpdate: () => {
-            setProjects(true)
+          setProjects(true)
         }
       }
     })
     gsap.to('section', {
       scrollTrigger: {
-        trigger: '.parallax',
+        trigger: '.skills',
         once: true,
+
         start: 'top 20%',
-        markers: { startColor: "green", endColor: "yellow", fontSize: "12px" },
-        onUpdate: () => {
-            setParallax(true)
+        onEnter: () => {
+            setSkills('Frontend')
         }
       }
     })
+
+
+
 
 
   })
@@ -64,6 +71,7 @@ function Main() {
     <div className="App">
       <div className="row">
         <Parallax parallax={parallax} />
+        <About about={about} />
         <Projects projects={projects} />
         <Skills skills={skills} />
 
---
+++ src/components/about/index.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+import { Container } from 'react-grid-system';
+import gsap from 'gsap'
+
+
+function About(props){
+    useEffect(() => {
+        if (props.about === true) {
+            console.log('true')
+            const description = document.querySelector('.description > p');
+            gsap.set(description, { autoAlpha: 0 });
+            gsap.fromTo(description, { y: '-=50', autoAlpha: 0 }, { y: 0, autoAlpha: 1, stagger: .3, duration: .5, ease: 'easeInOut' })
+        }
+
+    }, [props.about])
+    return(
+        <section className="about" >
+            <h2>About me</h2>
+            <Container className="description">
+                <p>dfsdf</p>
+            </Container>
+        </section>
+    );
+}
+
+export default About
